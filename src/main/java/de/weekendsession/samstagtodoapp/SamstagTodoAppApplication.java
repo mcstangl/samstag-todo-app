0aae3e957523e878f4ea62bef7117a22ef2a447b
Matthias Stangl <fazmaz@gmx.de>
2021-08-07 10:39:34 +0200

    initial commit

---
+++ src/main/java/de/weekendsession/samstagtodoapp/SamstagTodoAppApplication.java
@@ -0,0 +1,13 @@
+package de.weekendsession.samstagtodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamstagTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamstagTodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/weekendsession/samstagtodoapp/SamstagTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.weekendsession.samstagtodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamstagTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
