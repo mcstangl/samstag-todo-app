ada44c6f7bca69e93e16c2980205ccffea4bc72b
Matthias Stangl <fazmaz@gmx.de>
2021-08-07 10:45:01 +0200

    added todo model and status enum

---
+++ src/main/java/de/weekendsession/samstagtodoapp/enums/Status.java
@@ -0,0 +1,8 @@
+package de.weekendsession.samstagtodoapp.enums;
+
+public enum Status {
+
+    OPEN, IN_PROGRESS, REVIEW, DONE, DELETED;
+
+
+}
---
+++ src/main/java/de/weekendsession/samstagtodoapp/model/TodoItem.java
@@ -0,0 +1,83 @@
+package de.weekendsession.samstagtodoapp.model;
+
+import de.weekendsession.samstagtodoapp.enums.Status;
+
+import java.util.Objects;
+
+public class TodoItem {
+
+    private final String id;
+    private String title;
+    private String description;
+    private String timestamp;
+    private Status status;
+
+
+    public TodoItem(String id, String title, String description, String timestamp, Status status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.timestamp = timestamp;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TodoItem todoItem = (TodoItem) o;
+        return Objects.equals(id, todoItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "TodoItem{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", timestamp='" + timestamp + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
